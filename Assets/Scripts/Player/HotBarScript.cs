dca1b63afbb81531b26a38ad862cff55cc97885e
Michael Butkevicius <mjbutkevicius@gmail.com>
2024-05-14 15:29:15 -0500

    Hotbar Functionality Update
    Added more featuers to hotbar. Can cycle through, add, drop, and swap items in the hotbar

--- Assets/Scripts/Player/HotBarScript.cs
+++ Assets/Scripts/Player/HotBarScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,6 +11,9 @@ public class HotBarScript : MonoBehaviour
     public int hotbarSize = 5;
     public int currentSlot = 0;
     public Item[] hotbarSlots;
+
+    public bool inItemRange = false;
+    public bool pickupButtonPressed = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,19 +24,75 @@ public class HotBarScript : MonoBehaviour
         }
         //RefreshUI();
         
-        
-        // might not need the slotsUI and if so I'll use this one
         hotbarSlots = new Item[hotbarSize];
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        if (UserInput.instance.controls.Hotbar.Pickup.WasPressedThisFrame()){
+            pickupButtonPressed = true;
+        }
+        else if (UserInput.instance.controls.Hotbar.ScrollRight.WasPressedThisFrame()){
+            ScrollRight();
+        }
+        else if (UserInput.instance.controls.Hotbar.ScrollLeft.WasPressedThisFrame()){
+            ScrollLeft();
+        }
+        else if (UserInput.instance.controls.Hotbar.Drop.WasPressedThisFrame()){
+            DropItem(currentSlot);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other){
+        //inItemRange = true;
+
+        if (pickupButtonPressed && other.CompareTag("Pickup")){ //&& hotbarSlots[currentSlot] == null){
+            pickupButtonPressed = false;
+
+            Item item = other.GetComponent<Item>();
+            Debug.Log("Item name" + item.name);
+
+            int nextSlotIndex = NextHotbarSlotIndex();
+
+            // add to empty hotbar slot
+            if ((nextSlotIndex != currentSlot) || (nextSlotIndex == currentSlot && hotbarSlots[currentSlot] == null)){
+                AddToHotbar(item, nextSlotIndex);
+
+                other.transform.parent.gameObject.SetActive(false);
+            }
+            // swap item for item in current slot
+            else {
+                SwapHotbarItem(item, nextSlotIndex);
+                other.transform.parent.gameObject.SetActive(false);
+                //DropItem(nextSlotIndex);
+            }
+        }
+        //Debug.Log(hotbarSlots[currentSlot]);
+    }
+
     public void RefreshUI(){
-        Debug.Log(hotbarSlots[0].GetComponent<SpriteRenderer>().sprite);
+        //Debug.Log(hotbarSlots[0].GetComponent<SpriteRenderer>().sprite);
         for (int i = 0; i < slotsUI.Length; i++){
             if (hotbarSlots[i] != null){
                 slotsUI[i].transform.GetChild(0).GetComponent<Image>().sprite = hotbarSlots[i].GetComponent<SpriteRenderer>().sprite;
-                Debug.Log("Are we getting an image: " + hotbarSlots[i].GetComponent<SpriteRenderer>().sprite);
+                //Debug.Log("Are we getting an image: " + hotbarSlots[i].GetComponent<SpriteRenderer>().sprite);
+            }
+            // hot bar slot is null
+            else {
+                slotsUI[i].transform.GetChild(0).GetComponent<Image>().sprite = null;
             }
         }
+    }
+
+    public int NextHotbarSlotIndex(){
+        for (int i = 0; i < hotbarSlots.Length; i++){
+            if (hotbarSlots[i] == null){
+                return i;
+            }
+        }
+
+        return currentSlot;
     }
 
     public void AddToHotbar(Item item, int slotIndex){
@@ -44,36 +104,38 @@ public class HotBarScript : MonoBehaviour
         }
 
         RefreshUI();
+    }
 
-        if (currentSlot != 4){
+    public void SwapHotbarItem(Item item, int slotIndex){
+        DropItem(slotIndex);
+        AddToHotbar(item, slotIndex);
+    }
+
+    private void DropItem(int slotIndex){
+        if (hotbarSlots[slotIndex] != null){
+            hotbarSlots[slotIndex].transform.parent.gameObject.SetActive(true);
+            hotbarSlots[slotIndex] = null;
+        }
+        RefreshUI();
+    }
+
+    private void ScrollRight(){
+        if (currentSlot == hotbarSlots.Length - 1){
+            currentSlot = 0;
+        }
+        else{
             currentSlot++;
         }
+        Debug.Log("currentSlot: " + currentSlot);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //DropItem();
-    }
-
-    private void OnTriggerEnter2D(Collider2D other){
-        if (other.CompareTag("Pickup") && hotbarSlots[currentSlot] == null){
-            Item item = other.GetComponent<Item>();
-            Debug.Log("Item name" + item.name);
-
-            if (hotbarSlots[currentSlot] == null){
-                AddToHotbar(item, currentSlot);
-
-                other.transform.parent.gameObject.SetActive(false);
-            }
+    private void ScrollLeft(){
+        if (currentSlot == 0){
+            currentSlot = hotbarSlots.Length - 1;
         }
-        Debug.Log(hotbarSlots[currentSlot]);
-    }
-
-    private void DropItem(){
-        if (hotbarSlots[currentSlot] != null){
-            //hotbarSlots[currentSlot].gameObject.SetActive(true);
-            //hotbarSlots[currentSlot] = null;
+        else{
+            currentSlot--;
         }
+        Debug.Log("currentSlot: " + currentSlot);
     }
 }
